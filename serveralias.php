02a7cb856ca2c27283203cb145d3f0d0810b4846
mfl <mfl@netspan.ch>
2012-04-22 15:22:27 +0000

    add send_info support

--- serveralias.php
+++ serveralias.php
@@ -65,51 +65,6 @@ class ServerAliasBlock extends BasicBlock
         }
 
         return $data;
-
-        /*
-        if (isset($_COOKIE['Xaraya_redirect'])) {
-        
-            // We already performed a redirect
-            $redirect = unserialize($_COOKIE['Xaraya_redirect']);
-            $data['redirect_target'] = $redirect['target'];
-            $data['redirect_source'] = $redirect['source'];
-            $data['redirect_language'] = $redirect['lang'];
-            
-            // Delete the cookie for good order
-            setcookie('Xaraya_redirect', '', time()-3600);
-        } else {
-        
-            // Set up the redirect
-            foreach ($this->redirects as $redirect) {
-                $source = trim($redirect['source']);
-                $target = trim($redirect['target']);
-                if (strpos(xarServer::getCurrentURL(),$source) === 0) {
-                    // Poor man's sanity check
-                    if ((strpos($target,$source) === 0) || (strpos($source,$target) === 0) ) break;
-                    $data['target'] = str_replace($source,$target,xarServer::getCurrentURL());
-                    $data['source'] = $redirect['source'];
-                    $data['language'] = $redirect['lang'];
-                    
-                    // Save a cookie with the redirect information
-                    setcookie('Xaraya_redirect', serialize($redirect), time()+3600);                
-                    break;
-                }
-            }
-        }
-
-        // If we already redirected and sent the redirect info, then grab it here
-        if(!xarVarFetch('redirect_s',  'str',   $redirect_s,     '',       XARVAR_NOT_REQUIRED)) {return;}
-        if(!xarVarFetch('redirect_l',  'str',   $redirect_l,     '',       XARVAR_NOT_REQUIRED)) {return;}
-        if (!empty($redirect_s)) {
-            try {
-                $data['source'] = $redirect_s;
-                $data['language'] = $redirect_l;
-                $data['postredirect'] = 1;
-            } catch (Exception $e) {}
-        }
-
-        return $data;
-        */
     }
 }
 ?>
